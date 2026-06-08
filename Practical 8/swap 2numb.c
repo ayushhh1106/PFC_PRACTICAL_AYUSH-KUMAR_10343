6617f1b9782606e6c2acbbeec4d16e8f2d91383c
Ayush Kumar <ayushhh.1106@gmail.com>
2026-06-08 22:32:20 +0530

    Update swap 2numb.c

--- Practical 8/swap 2numb.c
+++ Practical 8/swap 2numb.c
@@ -1,23 +1,18 @@
 #include <stdio.h>
-void swap(int*a,int*b){
-    //Ayush kumar_10343
-    int temp;
-    temp =*a;
-    *a=*b;
-    *b=temp;
-}
+
 int main() {
-    int x, y;
-    printf("Enter two numbers:\n");
-    scanf("%d %d",&x,&y);
-    printf("Before swapping:x=%d,y=%d\n",x,y);
-    swap(&x, &y); 
-    printf("After swapping: x=%d, y=%d\n",x,y);
+    int a, b, temp;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    temp = a;
+    a = b;
+    b = temp;
+
+    printf("After swapping:\n");
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
+
     return 0;
 }
-//output
-//Enter two numbers:
-//5 7
-//Before swapping:x=5,y=7
-
-//After swapping: x=7, y=5
