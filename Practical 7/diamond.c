e983fba1dd6b8dbccfd671af89086821e2d087e9
Ayush Kumar <ayushhh.1106@gmail.com>
2026-06-12 22:34:13 +0530

    Update diamond.c

--- Practical 7/diamond.c
+++ Practical 7/diamond.c
@@ -1,37 +1,39 @@
 #include <stdio.h>
-int main(){
-    //Ayush kumar_10343
-    // E
+
+int main() {
+    // Ayush Kumar_10343
+    // Diamond Pattern Program in C
+
     int n;
-    printf("Enter the value of n : ");
+
+    printf("Enter the value of n: ");
     scanf("%d", &n);
-    for (int i=1;i<=n;i++) {
-        for (int space = 1; space<=n-i; space++) {
+
+    for (int i = 1; i <= n; i++) {
+        for (int space = 1; space <= n - i; space++) {
             printf(" ");
         }
-        for (int star=1; star<=2*i-1; star++) {
+
+        for (int star = 1; star <= 2 * i - 1; star++) {
             printf("*");
         }
+
         printf("\n");
     }
-    for (int i=n-1;i>=1;i--) {
-        for (int space=1; space<=n-i; space++) {
+
+    for (int i = n - 1; i >= 1; i--) {
+        for (int space = 1; space <= n - i; space++) {
             printf(" ");
         }
-        for (int star=1; star<=2*i-1 ;star++) {
+
+        for (int star = 1; star <= 2 * i - 1; star++) {
             printf("*");
         }
+
         printf("\n");
     }
+
+    printf("\nPattern printed successfully!\n");
+
     return 0;
 }
-//output
-//Enter the value of n: 4
-//   *
- // ***
- //*****
-//*******
- //*****
-  //***
-
-  // *
